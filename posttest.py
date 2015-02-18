6235f8f503f1e5fe8c1bd79dbf304e3efda6cec2
Jim Dennis <James_Dennis@symantec.com>
2015-02-18 12:48:35 -0800

    posttest: add __main__ and track errors

--- posttest.py
+++ posttest.py
@@ -2,19 +2,26 @@
 from loremipsum import get_sentences
 import requests
 import time
-start = time.time() 
+start = time.time()
 generate_elapsed = time.time() - start
+err_count = 0
 tstsuite = [{'name':x.split()[0],'message':x} for x in get_sentences(1000)]
 def testpost(tst):
     r = requests.post('http://localhost:8080/confirmation', data=tst)
     if not r.ok:
-        print 'No OK:', nm
+        print 'Not OK:', nm
+        err_count += 1
     return r
-start = time.time(); 
-for each in tstsuite:
-    testpost(each)
-elapsed = time.time() - start 
 
-n =  len(tstsuite)
-print "Took %s seconds to generate %d tests" % (generate_elapsed, n)
-print "Took %s seconds to post %d tests (%g/second)" % (elapsed, n, n/elapsed)
+if __name__ == '__main__':
+    start = time.time();
+    for each in tstsuite:
+        testpost(each)
+    elapsed = time.time() - start
+
+    n =  len(tstsuite)
+    print 'Took %s seconds to generate %d tests' % (generate_elapsed, n)
+    print 'Took %s seconds to post %d tests (%g/second)' % (elapsed, n, n/elapsed)
+    if err_count:
+        print 'There were %d error reported in testpost()' % err_count
+
